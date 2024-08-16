7bb0c29b10cfb721bc79e491024ce86d9ef27cc2
ShyavanS <shyavans@gmail.com>
2024-08-16 00:02:16 -0400

    formatting

--- graveler_old.py
+++ graveler_old.py
@@ -12,10 +12,13 @@ maxOnes = 0
 
 while numbers[0] < 177 and rolls < 1000000000:
     numbers = [0, 0, 0, 0]
+    
     for i in repeat(None, 231):
         roll = random.choice(items)
         numbers[roll - 1] = numbers[roll - 1] + 1
+    
     rolls = rolls + 1
+    
     if numbers[0] > maxOnes:
         maxOnes = numbers[0]
 
@@ -23,6 +26,6 @@ tEnd = time()
 
 tElapsed = tEnd - tStart
 
-print("Highest Ones Roll:", maxOnes)
-print("Number of Roll Sessions: ", rolls)
+print(f"Highest Ones Roll: {maxOnes}")
+print(f"Number of Roll Sessions: {rolls}")
 print(f"Runtime: {tElapsed:.3f} s")
